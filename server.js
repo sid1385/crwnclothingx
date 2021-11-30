32375bdb6aaafbaccbc05e61531b97b786a31640
siddhesh <siddhesh@proost.in>
2021-11-30 23:04:26 +0530

    Version 1.4

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-if (process.env.NODE_ENV === "production")
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
-
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 app.listen(port, (error) => {
   if (error) throw error;
   console.log("the server running on port" + port);
